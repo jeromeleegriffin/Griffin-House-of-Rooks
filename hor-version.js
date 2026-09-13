78ed7b6fe2755ab57fe24bc9c76ffeb7d57eb44f
jeromeleegriffin <57483100+jeromeleegriffin@users.noreply.github.com>
2026-09-13 07:44:13 -0400

    Add files via upload

--- hor-version.js
+++ hor-version.js
@@ -1,3 +1,3 @@
 /* House of Rooks live version beacon. Loaded cross-origin by downloaded copies. */
-window.HOR_LIVE_VERSION = 202;
-window.HOR_BUILD = 323;
+window.HOR_LIVE_VERSION = 324;
+window.HOR_BUILD = 324;
--- sw.js
+++ sw.js
@@ -2,20 +2,20 @@
  * Author: Jerome Griffin
  * Copyright (c) 2026 Jerome Griffin / Griffin House
  */
-const CACHE = 'house-of-rooks-v202-backs2';
+const CACHE = 'house-of-rooks-v324';
 const SHELL = [
   './',
   './index.html',
   './rules.js',
-  './rules.js?v=202',
+  './rules.js?v=324',
   './bots.js',
-  './bots.js?v=202',
+  './bots.js?v=324',
   './game.js',
-  './game.js?v=202',
+  './game.js?v=324',
   './polish.js',
-  './polish.js?v=202',
+  './polish.js?v=324',
   './style.css',
-  './style.css?v=202',
+  './style.css?v=324',
   './hor-version.js',
   './splash-battle.png',
   './wait-nest-portrait.jpg',
