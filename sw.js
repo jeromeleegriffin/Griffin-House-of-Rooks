2d3450e5ec7ef8b8bae24c9046dfe1fb6defa70d
jeromeleegriffin <57483100+jeromeleegriffin@users.noreply.github.com>
2026-09-13 07:15:43 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Author: Jerome Griffin
  * Copyright (c) 2026 Jerome Griffin / Griffin House
  */
-const CACHE = 'house-of-rooks-v202-backs';
+const CACHE = 'house-of-rooks-v202-navy';
 const SHELL = [
   './',
   './index.html',
