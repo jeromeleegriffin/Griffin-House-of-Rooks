cb46592a8d0ecfe66d192facf11d937ac08d3531
jeromeleegriffin <57483100+jeromeleegriffin@users.noreply.github.com>
2026-09-13 07:48:51 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,20 +2,20 @@
  * Author: Jerome Griffin
  * Copyright (c) 2026 Jerome Griffin / Griffin House
  */
-const CACHE = 'house-of-rooks-v324';
+const CACHE = 'house-of-rooks-v325';
 const SHELL = [
   './',
   './index.html',
   './rules.js',
-  './rules.js?v=324',
+  './rules.js?v=325',
   './bots.js',
-  './bots.js?v=324',
+  './bots.js?v=325',
   './game.js',
-  './game.js?v=324',
+  './game.js?v=325',
   './polish.js',
-  './polish.js?v=324',
+  './polish.js?v=325',
   './style.css',
-  './style.css?v=324',
+  './style.css?v=325',
   './hor-version.js',
   './splash-battle.png',
   './wait-nest-portrait.jpg',
